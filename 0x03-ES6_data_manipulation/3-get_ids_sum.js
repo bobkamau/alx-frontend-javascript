b9c27503f9a258b210695160184ac1fc6b482b4a
bobkamau <Bob@bobbymwangi.com>
2023-01-03 21:44:17 +0300

    3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(lst) {
-
-	  return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
-
+  return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
 }
