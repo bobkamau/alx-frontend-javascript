55d1900c40b39e3232e9c24daf43610e7fd96002
bobkamau <Bob@bobbymwangi.com>
2023-01-03 22:13:44 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,27 +1,14 @@
 export default function cleanSet(set, startString) {
+  let text = '';
+  const array = [];
 
-	  let text = '';
-
-	  const array = [];
-
-
-
-	  if (startString && typeof startString === 'string') {
-
-		      for (const element of set) {
-
-			            if (element && element.startsWith(startString)) {
-
-					            array.push(element.slice(startString.length));
-
-					          }
-
-			          }
-
-		      text = array.join('-');
-
-		    }
-
-	  return text;
-
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
+    }
+    text = array.join('-');
+  }
+  return text;
 }
