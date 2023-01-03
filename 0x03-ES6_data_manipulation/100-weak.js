9b1c2196ebee7701059bf88719b4505938e7fbc3
bobkamau <Bob@bobbymwangi.com>
2023-01-03 22:28:39 +0300

    11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,11 @@
 export const weakMap = new WeakMap();
-
 let count = 1;
 
-
-
 export function queryAPI(endPoint) {
-
-	  weakMap.set(endPoint, count);
-
-	  count += 1;
-
-	  const queryCount = weakMap.get(endPoint);
-
-	  if (queryCount >= 5) {
-
-		      throw new Error('Endpoint load is high');
-
-		    }
-
+  weakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
