5652539005d18b840dfa840a5ce9d887261e930e
bobkamau <Bob@bobbymwangi.com>
2023-01-04 19:03:48 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,13 +1,77 @@
+// Teacher interface
 interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [propName: string]: any;
+}
 
-	  readonly firstName: string;
+// Directors interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
-	    readonly lastName: string;
+// Creating a teacher and a director
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
 
-	      fullTimeEmployee: boolean;
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
 
-	        location: string;
+// returns a Teacher's name in this format: S. Cheung
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
 
-		  yearsOfExperience?: number;
+console.log(printTeacher('Dora', 'Montero'));
 
-		    [propName: string]: any;
+// class description interface
+interface classInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// constructor description interface
+interface classConstructor {
+  new (firstName: string, lastName: string): classInterface;
+}
+
+// Creating class and constructor through interfaces 
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// creating instance of StudentClass
+const studentClass: StudentClass = new StudentClass('sofia', 'cheung');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
