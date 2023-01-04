736f9c7031c9d91539b6f4167d47edb0cc3132dd
bobkamau <robertmwangi369@gmail.com>
2023-01-04 08:02:58 -0800

    1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,13 @@
+interface Teacher {
+
+	  readonly firstName: string;
+
+	    readonly lastName: string;
+
+	      fullTimeEmployee: boolean;
+
+	        location: string;
+
+		  yearsOfExperience?: number;
+
+		    [propName: string]: any;
