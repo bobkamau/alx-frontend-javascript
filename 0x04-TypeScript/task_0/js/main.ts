2e066ebc0c229cf9c926a3573b3d17e2d1609fc2
bobkamau <Bob@bobbymwangi.com>
2023-01-04 18:37:58 +0300

    main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,9 +1,40 @@
+/** Renders table based on a list of objects */
+
 interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
 
-	  firstName: string;
+const studentA: Student = {
+  firstName: 'senait',
+  lastName: 'alemayehu',
+  age: 24,
+  location: 'ethiopia',
+}
 
-	    lastName: string;
+const studentB: Student = {
+  firstName: 'gedion',
+  lastName: 'hailu',
+  age: 21,
+  location: 'ethiopia',
+}
 
-	      age: number;
+const studentList = [studentA, studentB];
 
-	        location: string;
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
